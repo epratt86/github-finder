686c0a970b460afa3216ac3a9f8ed84116ef4859
Eric Pratt <ericpratt86@gmail.com>
2019-02-07 18:45:36 -0800

    add debounce functionality

--- app.js
+++ app.js
@@ -6,13 +6,31 @@ const ui = new UI();
 // Search input
 const searchUser = document.getElementById("searchUser");
 
-searchUser.addEventListener("keyup", e => {
-  // Get input text
-  const userText = e.target.value;
+// a debounce function will only call `callback` after `ms` milliseconds have
+// passed since `callback` was previously called.
+const debounce = (callback, ms) => {
+  let timeout;
+  return function() {
+    const args = arguments;
+    const context = this;
+    clearTimeout(timeout);
+    timeout = setTimeout(function() {
+      callback.apply(context, args);
+    }, ms);
+  };
+};
 
-  if (userText !== "") {
+// a place to store previous searches
+let previousInput;
+
+// a debounced function that will only execute 750 milliseconds after it was
+// previously called (so, 750ms after the user has stopped typing).
+const handleSearch = debounce(function(e) {
+  const currentInput = e.target.value;
+  // make sure current input isn't blank
+  if (currentInput !== "") {
     // Make HTTP call
-    github.getUser(userText).then(data => {
+    github.getUser(currentInput).then(data => {
       if (data.profile.message === "Not Found") {
         // Show alert
         ui.showAlert("User not found", "alert alert-danger");
@@ -26,4 +44,9 @@ searchUser.addEventListener("keyup", e => {
     // Clear
     ui.clearProfile();
   }
-});
+  // update previous input
+  previousInput = currentInput;
+}, 750);
+
+// finally, attach the event to the search bar.
+searchUser.addEventListener("keyup", handleSearch);
